b0ca427ed244110eed4f6604cf155f98e82e1aef
Ahmed iyanuoluwa <127423641+iyanu752@users.noreply.github.com>
2025-06-19 09:01:38 +0100

    cors-fix-live

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { PORT } from './common/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://estate-run-client.vercel.app',
     Credentials: true,
   });
   await app.listen(PORT ?? 3000);
